532cf88abe37dfddb0232ad49742bc51cd87e81b
Sammyduzit <sammy.duzit999@gmail.com>
2026-03-11 20:30:03 +0100

    chore: format small whitespace changes

--- tests/integration/test_stm_to_ltm_flow.py
+++ tests/integration/test_stm_to_ltm_flow.py
@@ -65,6 +65,7 @@ async def test_stm_to_ltm_flow(db_session):
     # Create a few conversation messages
     from sqlalchemy import select
     from sqlalchemy.orm import selectinload
+
     from app.models.message import Message
 
     messages = []
